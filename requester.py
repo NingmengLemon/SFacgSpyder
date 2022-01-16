7c2b4dfbef5bf6ce688c9f41614585d580e3172e
Hana-Shirosaki <64351595+Elaina-Alex@users.noreply.github.com>
2022-01-16 11:24:48 +0800

    重构章节下载逻辑

--- requester.py
+++ requester.py
@@ -1,53 +1,56 @@
-from urllib import request, parse, error
+import gzip
+import logging
 import os
 import re
-import sys
-import time
-import json
-import zlib,gzip
+import zlib
 from io import BytesIO
-import logging
+from urllib import request, parse
 
-#requester's pre-data
+# requester's pre-data
 user_name = os.getlogin()
 fake_headers_get = {
     'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',  # noqa
     'Accept-Charset': 'UTF-8,*;q=0.5',
     'Accept-Encoding': 'gzip,deflate,sdch',
     'Accept-Language': 'en-US,en;q=0.8',
-    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.110 Safari/537.36',  # noqa
-    'Referer':'https://book.sfacg.com/'
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) '
+                  'Chrome/96.0.4664.110 Safari/537.36',
+    # noqa
+    'Referer': 'https://book.sfacg.com/'
 }
 
 fake_headers_post = {
-    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.74 Safari/537.36 Edg/79.0.309.43'
-    }
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) '
+                  'Chrome/79.0.3945.74 Safari/537.36 Edg/79.0.309.43 '
+}
 
 fake_headers_iosapp = {
-        'User-Agent': 'boluobao/4.5.52(iOS;14.0)/appStore',
-        'Host': 'api.sfacg.com',
-        'Authorization': 'Basic YXBpdXNlcjozcyMxLXl0NmUqQWN2QHFlcg=='
-    }
+    'User-Agent': 'boluobao/4.5.52(iOS;14.0)/appStore',
+    'Host': 'api.sfacg.com',
+    'Authorization': 'Basic YXBpdXNlcjozcyMxLXl0NmUqQWN2QHFlcg=='
+}
 
-chrome_path = 'C:\\Users\\%s\\AppData\\Local\\Google\\Chrome\\Application\\chrome.exe'%user_name
+chrome_path = 'C:\\Users\\%s\\AppData\\Local\\Google\\Chrome\\Application\\chrome.exe' % user_name
 
 timeout = 15
 
+
 def _replaceChr(text):
-    repChr = {'/':'／',
-              '*':'＊',
-              ':':'：',
-              '\\':'＼',
-              '>':'＞',
-              '<':'＜',
-              '|':'｜',
-              '?':'？',
-              '"':'＂'}
+    repChr = {'/': '／',
+              '*': '＊',
+              ':': '：',
+              '\\': '＼',
+              '>': '＞',
+              '<': '＜',
+              '|': '｜',
+              '?': '？',
+              '"': '＂'}
     for t in list(repChr.keys()):
-        text = text.replace(t,repChr[t])
+        text = text.replace(t, repChr[t])
     return text
 
-#requester
+
+# requester
 def _ungzip(data):
     """Decompresses data for Content-Encoding: gzip.
     """
@@ -55,20 +58,23 @@ def _ungzip(data):
     f = gzip.GzipFile(fileobj=buffer)
     return f.read()
 
+
 def _undeflate(data):
     """Decompresses data for Content-Encoding: deflate.
     (the zlib compression is used.)
     """
     decompressobj = zlib.decompressobj(-zlib.MAX_WBITS)
-    return decompressobj.decompress(data)+decompressobj.flush()
+    return decompressobj.decompress(data) + decompressobj.flush()
+
 
 def _dict_to_headers(dict_to_conv):
     keys = list(dict_to_conv.keys())
     values = list(dict_to_conv.values())
     res = []
     for i in range(len(keys)):
-        res.append((keys[i],values[i]))
+        res.append((keys[i], values[i]))
     return res
+
 
 def _get_response(url, headers=fake_headers_get):
     opener = request.build_opener()
@@ -86,75 +92,85 @@ def _get_response(url, headers=fake_headers_get):
     elif response.info().get('Content-Encoding') == 'deflate':
         data = _undeflate(data)
     response.data = data
-    logging.debug('Get Response from: '+url)
+    logging.debug('Get Response from: ' + url)
     return response
 
-def _post_request(url,data,headers=fake_headers_post):
+
+def _post_request(url, data, headers=fake_headers_post):
     opener = request.build_opener()
     params = parse.urlencode(data).encode()
     if headers:
-        response = opener.open(request.Request(url,data=params,headers=headers), timeout=timeout)
+        response = opener.open(request.Request(url, data=params, headers=headers), timeout=timeout)
     else:
-        response = opener.open(request.Request(url,data=params), timeout=timeout)
+        response = opener.open(request.Request(url, data=params), timeout=timeout)
     data = response.read()
     if response.info().get('Content-Encoding') == 'gzip':
         data = _ungzip(data)
     elif response.info().get('Content-Encoding') == 'deflate':
         data = _undeflate(data)
     response.data = data
-    logging.debug('Post Data to {} with Params {}'.format(url,str(params)))
+    logging.debug('Post Data to {} with Params {}'.format(url, str(params)))
     return response
 
-def post_data_str(url,data,headers=fake_headers_post,encoding='utf-8'):
-    response = _post_request(url,data,headers)
+
+def post_data_str(url, data, headers=fake_headers_post, encoding='utf-8'):
+    response = _post_request(url, data, headers)
     return response.data.decode(encoding, 'ignore')
 
-def post_data_bytes(url,data,headers=fake_headers_post,encoding='utf-8'):
-    response = _post_request(url,data,headers)
+
+def post_data_bytes(url, data, headers=fake_headers_post, encoding='utf-8'):
+    response = _post_request(url, data, headers)
     return response.data
+
 
 def get_content_str(url, encoding='utf-8', headers=fake_headers_get):
     content = _get_response(url, headers=headers).data
     data = content.decode(encoding, 'ignore')
     return data
 
+
 def get_content_bytes(url, headers=fake_headers_get):
     content = _get_response(url, headers=headers).data
     return content
 
-def get_redirect_url(url,headers=fake_headers_get):
-    return request.urlopen(request.Request(url,headers=headers),None).geturl()
 
-#Download Operation
-def download_common(url,tofile,progressfunc=None,headers=fake_headers_get):
+def get_redirect_url(url, headers=fake_headers_get):
+    return request.urlopen(request.Request(url, headers=headers), None).geturl()
+
+
+# Download Operation
+def download_common(url, tofile, progressfunc=None, headers=fake_headers_get):
     opener = request.build_opener()
     opener.addheaders = _dict_to_headers(headers)
     request.install_opener(opener)
-    request.urlretrieve(url,tofile,progressfunc)
+    request.urlretrieve(url, tofile, progressfunc)
 
-def convert_size(self,size):#单位:Byte
+
+def convert_size(self, size):  # 单位:Byte
     if size < 1024:
-        return '%.2f B'%size
+        return '%.2f B' % size
     size /= 1024
     if size < 1024:
-        return '%.2f KB'%size
+        return '%.2f KB' % size
     size /= 1024
     if size < 1024:
-        return '%.2f MB'%size
+        return '%.2f MB' % size
     size /= 1024
-    return '%.2f GB'%size
+    return '%.2f GB' % size
+
 
 def second_to_time(sec):
     h = sec // 3600
     sec = sec % 3600
     m = sec // 60
     s = sec % 60
-    return '%d:%02d:%02d'%(h,m,s)
+    return '%d:%02d:%02d' % (h, m, s)
+
 
 def _is_url(url):
     if re.match(r'^https?:/{2}\w.+$', url):
         return True
     else:
         return False
-        
-#load_local_cookies()
+
+# load_local_cookies()
--- sfacg.py
+++ sfacg.py
@@ -5,147 +5,143 @@ import time
 import os
 import sys
 import json
-import emoji
 
 version = '1.1.0'
 
+
+def filter_emoji(string: str) -> str:
+    # 过滤表情
+    try:
+        co = re.compile(u'[\U00010000-\U0010ffff]')
+    except re.error:
+        co = re.compile(u'[\uD800-\uDBFF][\uDC00-\uDFFF]')
+    return co.sub('', string)
+
+
 class UrlUnspecifiedError(Exception):
-    def __init__(self,url):
+    def __init__(self, url):
         self.msg = 'The URL \"{}\" is not the specified type.'.format(url)
 
     def __str__(self):
         return self.msg
 
-class VipChapterSkipError(Exception): #因为搞VIP章节太麻烦, 于是干脆不做的屑
+
+class VipChapterSkipError(Exception):  # 因为搞VIP章节太麻烦, 于是干脆不做的屑
     def __init__(self):
         self.msg = 'VIP Chapter skipped.'
 
     def __str__(self):
         return self.msg
 
+
 class ApiRequestError(Exception):
-    def __init__(self,code,msg):
+    def __init__(self, code, msg):
         self.code = code
         self.msg = msg
-        self._final_msg = 'Code {}: {}'.format(code,msg)
+        self._final_msg = 'Code {}: {}'.format(code, msg)
 
     def __str__(self):
         return self._final_msg
-        
+
+
 def check_login():
     url = req.get_redirect_url('https://passport.sfacg.com/')
     if 'Login.aspx' in url:
         return False
     else:
         return True
-    
+
+
 class Chapter(object):
-    def __init__(self,chapter_url):
-        self.url = chapter_url
-        if not self._check_chapter_url(chapter_url):
-            raise UrlUnspecifiedError(chapter_url)
-        source_code = req.get_content_str(chapter_url)
-        bs = BS(source_code,'html.parser')
-        #书本信息
-        self.is_vip_chapter = 'vip' in chapter_url
-        if self.is_vip_chapter:
-            raise VipChapterSkipError()
-        _bookinfo = bs.find('div',class_='crumbs clearfix').find_all('a')[-1]
-        self.bookname = _bookinfo.get_text()
-        self.novel_id = int(_bookinfo.attrs['href'][7:-1])
-        #章节内容
-        _content = bs.find('div',id='ChapterBody')
-        if self.is_vip_chapter:
-            self.content = _content.find('img',id='vipImgae')
-            if self.content:
-                self.content = BytesIO(req.get_content_bytes(self.content.attrsp['src']))
-        else:
-            self.content = '\u3000\u3000'+_content.get_text('\n\u3000\u3000').strip() if _content else None
-        #章节信息
-        _info = bs.find('div',class_='article-desc').find_all('span',class_='text')
-        self.info = [i.get_text() for i in _info]
-        self.writer = self.info[0][3:]
-        self.publish_time = self.info[1][5:]
-        self.word_number = int(self.info[2][3:])
-        #标题
-        self.title = bs.find('h1',class_='article-title').get_text()
-        #相邻章节url
-        _nearby_url = ['https://book.sfacg.com'+i[0] for i in re.findall(r'\<a href\=\"(.+)\" class\=\"btn normal\"\>(上|下)一章\</a\>',source_code)]
-        self.last_url = _nearby_url[0] if self._check_chapter_url(_nearby_url[0]) else None
-        self.next_url = _nearby_url[1] if self._check_chapter_url(_nearby_url[1]) else None
-        #目录页与详情页url
-        self.index_url = 'https://book.sfacg.com/Novel/{}/MainIndex/'.format(self.novel_id)
-        self.main_url = 'https://book.sfacg.com/Novel/{}/'.format(self.novel_id)
+    def __init__(self, chapter_url_list, book_name):
+        self.chapter_url_list = chapter_url_list
+        self.book_name = book_name
 
-    def _check_chapter_url(self,url):
-        if re.match(r'https?://book\.sfacg\.com/Novel/[0-9]+/[0-9]+/[0-9]+/?$',url) or re.match(r'https?\://book\.sfacg\.com/vip/c/[0-9]+',url):
-            return True
-        else:
-            return False
+    def download(self):
+        for chapter_url in self.chapter_url_list:
+            source_code = req.get_content_str(chapter_url)
+            bs = BS(source_code, 'html.parser')
+            _bookinfo = bs.find('div', class_='crumbs clearfix').find_all('a')[-1]
+            # 章节内容
+            _content = bs.find('div', id='ChapterBody')
+            content = '\u3000\u3000' + _content.get_text('\n\u3000\u3000').strip() if _content else None
+            # 章节信息
+            _info = bs.find('div', class_='article-desc').find_all('span', class_='text')
+            info = [i.get_text() for i in _info]
+            writer = info[0][3:]
+            publish_time = info[1][5:]
+            word_number = int(info[2][3:])
+            # 标题
+            chapter_title = filter_emoji(bs.find('h1', class_='article-title').get_text())
+            print('Get the chapter:', chapter_title)
+            with open(os.path.join('output', self.book_name + '.txt'), 'a', encoding='utf-8') as f:
+                f.write('\n\n\n' + chapter_title)
+                f.write('\nWord Number: ' + str(word_number))
+                f.write('\nPublish Time: ' + publish_time)
+                f.write('\n' + filter_emoji(content))
 
-    def next_chapter(self):
-        if self.next_url:
-            self.__init__(self.next_url)
-            return True
-        else:
-            return False
 
-class MainIndex(object):
-    def __init__(self,index_url=None,novel_id=None):
-        if novel_id:
-            self.url = index_url = 'https://book.sfacg.com/Novel/{}/MainIndex/'.format(novel_id)
-        else:
-            self.url = index_url
-        if not self.url:
-            raise RuntimeError('You must choose one parameter between index_url and novel_id.')
-        if not self._check_index_url(index_url):
-            raise UrlUnspecifiedError(index_url)
+class MainIndex:
+    def __init__(self, novel_id):
+        self.novel_id = novel_id
+        self.source_data = json.loads(
+            req.get_content_str('https://api.sfacg.com/novels/{}/dirs?expand=originNeedFireMoney%' \
+                                '2Ctags%2CsysTags%typeName'.format(self.novel_id), headers=req.fake_headers_iosapp))
 
-        self.novel_id = int(re.findall(r'Novel/([0-9]+)/',index_url)[0])
-        source_data = json.loads(req.get_content_str('https://api.sfacg.com/novels/{}/dirs?expand=originNeedFireMoney%2'\
-                                                     'Ctags%2CsysTags%typeName'.format(self.novel_id),headers=req.fake_headers_iosapp))
+    def get_main_index(self):
+        if self.source_data['status']['errorCode'] != 200:
+            raise ApiRequestError(self.source_data['status']['errorCode'],
+                                  self.source_data['status']['msg'])
+        main_index_data = self.source_data.get('data')
+        main_index_urls = []
+        for main_index_volume in main_index_data['volumeList']:
+            volume_id = main_index_volume.get('volumeId')
+            for chapter_list in main_index_volume['chapterList']:
+                if chapter_list['isVip']:
+                    return main_index_urls
+                chap_id = chapter_list.get('chapId')
+                main_index_urls.append(
+                    'https://book.sfacg.com/Novel/{}/{}/{}/'.format(self.novel_id, volume_id, chap_id)
+                )
+        return main_index_urls
+
+        # 直接使用isVip判断即刻，无需将判断复杂化
+        # 章节url可以通过接口返回值拼接，储存为list遍历下载，不用从html提取
+        # self.complete_index = [{
+        #     'volume_id': volume['volumeId'],
+        #     'title': volume['title'],
+        #     'chapters': [{
+        #         'chapter_id': chapter['chapId'],
+        #         'novel_id': chapter['novelId'],
+        #         'volume_id': chapter['volumeId'],
+        #         'title': chapter['title'],
+        #         'is_vip': chapter['isVip'],
+        #         'url': 'https://book.sfacg.com/Novel/{}/{}/{}/'.format(chapter['novelId'], chapter['volumeId'],
+        #                                                                chapter['chapId']),
+        #         'order': chapter['chapOrder']
+        #     } for chapter in volume['chapterList']]
+        #
+        #
+        # } for volume in data['volumeList']]
+
+
+class Book:
+    def __init__(self, novel_id=None):
+        if novel_id is None:
+            print('You must choose one parameter between main_url and novel_id')
+
+        # 提取novel_id
+        if 'htttp' not in novel_id:
+            self.novel_id = novel_id
+        else:
+            self.novel_id = int(re.findall(r'/([0-9]+)/?', novel_id)[0])
+        source_data = json.loads(
+            req.get_content_str('https://api.sfacg.com/novels/{}?expand=chapterCount%2CbigBgBanner%2CbigNovelCover' \
+                                '%2CtypeName%2Cintro%2Cfav%2Cticket'.format(novel_id), headers=req.fake_headers_iosapp))
         if source_data['status']['errorCode'] != 200:
-            raise ApiRequestError(source_data['status']['errorCode'],source_data['status']['msg'])
+            raise ApiRequestError(source_data['status']['errorCode'], source_data['status']['msg'])
         data = source_data['data']
 
-        self.complete_index = [{
-            'volume_id':volume['volumeId'],
-            'title':volume['title'],
-            'chapters':[{
-                'chapter_id':chapter['chapId'],
-                'novel_id':chapter['novelId'],
-                'volume_id':chapter['volumeId'],
-                'title':chapter['title'],
-                'is_vip':chapter['isVip'],
-                'url':'https://book.sfacg.com/Novel/{}/{}/{}/'.format(chapter['novelId'],chapter['volumeId'],chapter['chapId']),
-                'order':chapter['chapOrder']
-                } for chapter in volume['chapterList']]
-            } for volume in data['volumeList']]
-
-    def _check_index_url(self,url):
-        if re.match(r'https?://book\.sfacg\.com/Novel/[0-9]+/MainIndex/?$',url):
-            return True
-        else:
-            return False
-
-class Book(object):
-    def __init__(self,main_url=None,novel_id=None):
-        if novel_id:
-            self.url = main_url = 'https://book.sfacg.com/Novel/{}/'.format(novel_id)
-        else:
-            self.url = main_url
-        if not self.url:
-            raise RuntimeError('You must choose one parameter between main_url and novel_id.')
-        if not self._check_book_url(main_url):
-            raise UrlUnspecifiedError(main_url)
-        #提取novel_id
-        self.novel_id = int(re.findall(r'Novel/([0-9]+)/?',main_url)[0])
-        source_data = json.loads(req.get_content_str('https://api.sfacg.com/novels/{}?expand=chapterCount%2CbigBgBanner%2CbigNovelCover' \
-                                                     '%2CtypeName%2Cintro%2Cfav%2Cticket'.format(novel_id),headers=req.fake_headers_iosapp))
-        if source_data['status']['errorCode'] != 200:
-            raise ApiRequestError(source_data['status']['errorCode'],source_data['status']['msg'])
-        data = source_data['data']
-        
         self.bookname = data['novelName']
         self.cover_url = data['expand']['bigNovelCover']
         self.bgbanner = data['expand']['bigBgBanner']
@@ -162,8 +158,8 @@ class Book(object):
         self.desc = data['expand']['intro']
         self.index_url = 'https://book.sfacg.com/Novel/{}/MainIndex/'.format(self.novel_id)
 
-    def _check_book_url(self,url):
-        if re.match(r'https?://book\.sfacg\.com/Novel/[0-9]+/?$',url):
+    def _check_book_url(self, url):
+        if re.match(r'https?://book\.sfacg\.com/Novel/[0-9]+/?$', url):
             return True
         else:
             return False
@@ -171,92 +167,71 @@ class Book(object):
     def all_info(self):
         return vars(self)
 
-def _write_chapter(f,chapter):
-    f.write('\n'+emoji.demojize(chapter.title))
-    f.write('\nWord Number: '+str(chapter.word_number))
-    f.write('\nPublish Time: '+chapter.publish_time)
-    f.write('\n'+emoji.demojize(chapter.content))
-            
+
+def _write_chapter(f, chapter):
+    f.write('\n' + filter_emoji(chapter.title))
+    f.write('\nWord Number: ' + str(chapter.word_number))
+    f.write('\nPublish Time: ' + chapter.publish_time)
+    f.write('\n' + filter_emoji(chapter.content))
+
+
 if __name__ == '__main__':
-    #debugmode = bool('-debug' in sys.argv)
+    # debugmode = bool('-debug' in sys.argv)
     print('Welcome to SFacgSpyder!')
-    print('Current Version:',version)
+    print('Current Version:', version)
     print('Sorry, this spyder cannot get the VIP chapters for you yet, what a pity!')
     print('Special thanks to GitHub user Elaina-Alex for SFacg API.')
-    print('Input the chapter url here, and the spyder will start getting chapters at this chapter until meet the VIP chapter.')
+    print('Input the chapter url here, and the spyder will start getting chapters at this chapter until meet the VIP '
+          'chapter.')
     print('A typical chapter url is like this: https://book.sfacg.com/Novel/263035/389168/3319726/')
-    url = input('Url: ')
+    novel_id = input('please input the novel_id:')
     print('Collecting Data...')
-    try:
-        chapter = Chapter(url)
-        book = Book(novel_id=chapter.novel_id)
-    except UrlUnspecifiedError as e:
-        print(str(e))
-    except VipChapterSkipError as e:
-        print('VIP chapter not supported yet.')
-    except Exception as e:
-        print('Unexpected Error:',str(e))
-        #if debugmode:
-        #    raise e
-    else:
-        print('========== Book Info ==========')
-        print('Book Name:',book.bookname)
-        print('Book ID:',book.novel_id)
-        print('Writer:',book.author)
-        print('Contain VIP Chapter:',{True:'Yes',False:'No'}[book.is_vip])
-        print('===============================')
-        print('\nWorking Start.')
-        if not os.path.exists('./output/'):
-            os.mkdir('./output/')
-        #print('#breakpoint01')
-        with open('./output/'+book.bookname+'.txt','w+',encoding='utf-8',errors='ignore') as f:
-            f.write('《'+book.bookname+'》')
-            f.write('\nBy: '+book.author)
-            f.write('\nVIP Status: '+str(book.is_vip))
-            f.write('\nDescription:\n'+book.desc)
-            f.write('\n==========\nWorking Start: '+time.strftime("%Y-%m-%d %H:%M:%S",time.localtime()))
-            #print('#breakpoint02')
-            #开始
-            time.sleep(3)
-            _write_chapter(f,chapter)
-            word_counter = chapter.word_number
-            chapter_counter = 1
-            print('Get the chapter:',chapter.title)
-            error_counter = 0
-            #print('#breakpoint03')
-            while True:
-                if chapter.next_url:
-                    #print('#breakpoint-loop')
-                    try:
-                        chapter.next_chapter()
-                    except VipChapterSkipError:
-                        print('Task ends because of the VIP chapter.')
-                        break
-                    except Exception as e:
-                        print('Unexpected Error:',str(e))
-                        if error_counter <= 3:
-                            error_counter += 1
-                            print('Retrying...')
-                            continue
-                        else:
-                            print('Task is ended by error.')
-                            break
-                    else:
-                        error_counter = 0
-                        _write_chapter(f,chapter)
-                        word_counter += chapter.word_number
-                        chapter_counter += 1
-                        print('Get the chapter:',chapter.title)
-                    finally:
-                        time.sleep(2)
-                else:
-                    break
-            print('Task ended.')
-            f.write('\nTask Ended at '+time.strftime("%Y-%m-%d %H:%M:%S",time.localtime()))
-            f.write('\n{} chapters & {} words were gotten in total.'.format(chapter_counter,word_counter))
-        #print('#breakpoint04')
-        print('Thanks for Using.')
-        if input('Open the output folder? (Y/N):').lower == 'y':
-            os.system('explorer output')
-    finally:
-        sys.exit()
+
+    book = Book(novel_id)
+    catalogue = MainIndex(book.novel_id)
+
+    # try:
+    #     chapter = Chapter(novel_id)
+    #     book = Book(novel_id=chapter.novel_id)
+    # except UrlUnspecifiedError as e:
+    #     print(str(e))
+    # except VipChapterSkipError as e:
+    #     print('VIP chapter not supported yet.')
+    # except Exception as e:
+    #     print('Unexpected Error:', str(e))
+    # if debugmode:
+    #    raise e
+    # else:
+    print('========== Book Info ==========')
+    print('Book Name:', book.bookname)
+    print('Book ID:', book.novel_id)
+    print('Writer:', book.author)
+    print('Contain VIP Chapter:', {True: 'Yes', False: 'No'}[book.is_vip])
+    print('===============================')
+    print('\nWorking Start.')
+    if not os.path.exists('./output/'):
+        os.mkdir('./output/')
+    # print('#breakpoint01')
+    with open('./output/' + book.bookname + '.txt', 'a', encoding='utf-8', errors='ignore') as f:
+        f.write('《' + book.bookname + '》')
+        f.write('\nBy: ' + book.author)
+        f.write('\nVIP Status: ' + str(book.is_vip))
+        f.write('\nDescription:\n' + book.desc)
+        f.write('\n==========\nWorking Start: ' + time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()))
+        # print('#breakpoint02')
+        # 开始
+        # time.sleep(3)
+        # _write_chapter(f, chapter)
+        # word_counter = chapter.word_number
+        # chapter_counter = 1
+        # print('Get the chapter:', chapter.title)
+        # error_counter = 0
+
+        chapter = Chapter(catalogue.get_main_index(), book.bookname).download()
+        print('Task ended.')
+        f.write('\nTask Ended at ' + time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()))
+        # f.write('\n{} chapters & {} words were gotten in total.'.format(chapter_counter, word_counter))
+    # print('#breakpoint04')
+    print('Thanks for Using.')
+    if input('Open the output folder? (Y/N):').lower == 'y':
+        os.system('explorer output')
